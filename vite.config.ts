64ec1325a349acd9d92c26a06795fd5fe9ee7765
Lumi1717 <ayhahlam623@gmail.com>
2025-03-26 20:04:17 +0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "robust-bandicoot-qafila-c8800ffd.koyeb.app",  
+    ],
   },
   plugins: [
     react(),
